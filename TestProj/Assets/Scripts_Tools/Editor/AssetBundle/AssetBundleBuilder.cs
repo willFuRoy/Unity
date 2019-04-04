7de7f7b16fc9206e0b54b804632b2d451ba7cc83
fxc <fxc@xcarray.com>
2019-04-04 16:32:39 +0800

    U 整理工程

--- TestProj/Assets/Scripts_Tools/Editor/AssetBundle/AssetBundleBuilder.cs
+++ TestProj/Assets/Scripts_Tools/Editor/AssetBundle/AssetBundleBuilder.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class AssetBundleBuilder
 {
+    const string jsonPath = "Assets/_ProjConfig/assetbundle_cfg.json";
 
+    /// <summary>
+    /// 清除所有Assetbundle名称
+    /// </summary>
+    static void ClearAssetBundleNames()
+    {
+        var names = AssetDatabase.GetAllAssetBundleNames();
 
+        for (int i = 0, max = names.Length; i < max; ++i)
+        {
+            AssetDatabase.RemoveAssetBundleName(names[i], true);
+        }
+        Debug.LogError("清除完成");
+    }
 
+    /// <summary>
+    /// 计算资源主路径
+    /// </summary>
+    /// <param name="path"></param>
+    static void CalcMainPathAsset(string path)
+    {
 
-
+    }
 }
